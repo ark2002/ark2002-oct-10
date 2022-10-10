bb362fdba25fe0217da1b5bfbb9bca1861659421
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-10 18:32:51 +0530

    refactor : added VisibilityModal

--- src/front-end/components/PostModal/PostModal.js
+++ src/front-end/components/PostModal/PostModal.js
@@ -1,36 +1,56 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useOnClickOutside } from "../../hooks/onClickOutsideHook";
+import { VisibilityModal } from "../VisibilityModal/VisibilityModal";
 import "./PostModal.css";
 
 const PostModal = () => {
   const [wordCount, setWordCount] = useState(0);
+  const [isVisiblityModal, setIsVisiblityModal] = useState(false);
+  const [visiblity, setVisiblity] = useState("private");
+  const ref = useRef();
+
+  useOnClickOutside(ref, () => setIsVisiblityModal(false));
 
   return (
-    <div className="post-model__container flex__column-center">
-      <div className="post-model__top flex__row-center">
-        <h3 className="post-model__title">Create a new post</h3>
+    <div className="post-modal__container flex__column-center">
+      <div className="post-modal__top flex__row-center">
+        <h3 className="post-modal__title">Create a new post</h3>
         <span className="material-icons close__btn">close</span>
       </div>
-      <div className="post-model__details flex__row-center">
+      <div className="post-modal__details flex__row-center">
         <img
           src="https://picsum.photos/100"
           alt="user-profile-img"
           className="user__profile-img"
         />
         <h2 className="user__profile-name">John Doe</h2>
-        <div className="post__visibility-dropdown flex__row-center">
-          <span className="material-icons public__icon">public</span>
-          <h3 className="post__visiblity-selected">Public</h3>
+        <div
+          className="post__visibility-dropdown flex__row-center"
+          onClick={() => setIsVisiblityModal(!isVisiblityModal)}
+          ref={ref}
+        >
+          <span className="material-icons public__icon">
+            {visiblity === "public" ? "public" : "lock"}
+          </span>
+          <h3 className="post__visiblity-selected">
+            {visiblity === "public" ? "Public" : "Private"}
+          </h3>
           <span className="expand__btn material-icons">expand_more</span>
+          <VisibilityModal
+            visiblity={visiblity}
+            setVisiblity={setVisiblity}
+            isVisiblityModal={isVisiblityModal}
+          />
         </div>
       </div>
-      <div className="post__model-input flex__column-center">
+      <div className="post__modal-input flex__column-center">
         <textarea
-          className="post__model-textarea"
+          className="post__modal-textarea"
           placeholder="What's Happening?"
           onChange={(e) => setWordCount(e.target.value.length)}
         />
         <h3
-          className={`post__model-counter ${
+          className={`post__modal-counter ${
             wordCount > 250
               ? `counter--error`
               : wordCount >= 240
@@ -43,11 +63,11 @@ const PostModal = () => {
           {wordCount} / 250
         </h3>
       </div>
-      <div className="post__model-choices flex__row-center">
+      <div className="post__modal-choices flex__row-center">
         <span className="material-icons add-image__btn">image</span>
         <span className="material-icons add-emoji__btn">mood</span>
         <button
-          className={`post__model-postbtn flex__row-center ${
+          className={`post__modal-postbtn flex__row-center ${
             wordCount < 1 || wordCount > 250 ? `postbtn--disabled` : ``
           }`}
           disabled={wordCount < 1 || wordCount > 250}
