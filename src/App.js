faae1a20c4883e3285b3510258f9845d8a8fa825
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-10 17:12:11 +0530

    feat : created Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import { Main } from "./front-end/components/Main/Main";
 
 function App() {
   return (
     <div className="App">
-      Bleeep
+      <Main />
     </div>
   );
 }
---
+++ src/front-end/components/Main/Main.js
@@ -0,0 +1,12 @@
+import { PostModal } from "../PostModal/PostModal";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="main__container flex__column-center">
+      <PostModal />
+    </div>
+  );
+};
+
+export { Main };
